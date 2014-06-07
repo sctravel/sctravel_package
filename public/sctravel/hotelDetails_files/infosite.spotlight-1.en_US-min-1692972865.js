a131ff307f1310bea3b383ffc0b1ed06215ce817
XI Tu <tuxi1987@gmail.com>
2014-06-07 00:01:58 -0700

    adding hotel details

---
+++ public/sctravel/hotelDetails_files/infosite.spotlight-1.en_US-min-1692972865.js
@@ -0,0 +1,4 @@
+try{(function(a,b,c){b("#opm-floodlight").attr("src",a.opmFloodlightSrc);b("#meso-floodlight").attr("src",a.mesoFloodlightSrc);}(window.infosite.spotlight=window.infosite.spotlight||{},jQuery));}catch(exception){infosite.handleException(exception);}
+/*!  generated on 2014-06-06 14:46:59.959 PDT(-0700) in 0 ms  */
+
+/*!  served in 0 ms  */
---
+++ public/sctravel/hotelDetails_files/uitk-only-header-bundle-min-2059231131.js
@@ -0,0 +1,4 @@
+(function(){var d=document.getElementById("mobile-toggle-header-link"),a=document.getElementById("globalSiteNavigation"),c="responsive-menu-shown";if(window.define&&window.require&&window.define.amd){require(["jQuery","uitk"],function(f){var e=f(a).hasClass(c);f(d).on(uitk.clickEvent,function(g){f(a).toggleClass(c);});});}else{var b=$(a).hasClass(c);$(d).on(uitk.clickEvent,function(f){$(a).toggleClass(c);});}}());
+/*!  generated on 2014-06-06 17:18:08.672 PDT(-0700) in 0 ms  */
+
+/*!  served in 0 ms  */
